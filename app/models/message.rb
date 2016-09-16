51ed171af6a25474d3ada735fbeb27e3b4215da6
Ronny Almog <ronny2205@gmail.com>
2016-09-16 22:41:17 +0000

    more validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ApplicationRecord
   include ActiveModel::Validations
 
-  VALID_PHONE_NUMBER_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+  # VALID_PHONE_NUMBER_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
+  VALID_PHONE_NUMBER_REGEX = /\A\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}[ ]*\z/
 
   validates :phone_number, presence: { message: "You must provide a phone number." }
   validates_format_of :phone_number,
